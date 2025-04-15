079d235541a591bd30eb738835af28bb94593598
Bhim Gurung <gbhimsingh@ymail.com>
2025-04-15 11:24:26 -0400

    FullNameGenerator.java

---
+++ FullNameGenerator/src/main/java/com/pluralsight/FullNameApplication.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+import java.util.Scanner;
+public class FullNameApplication {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please Enter your full Name");
+        String fullName = "firstName middleName lastName, Suffix";
+
+        System.out.print("Please Enter your firstName: ");
+        String firstName = Scanner.nextLine();
+
+
+
+}
+}
