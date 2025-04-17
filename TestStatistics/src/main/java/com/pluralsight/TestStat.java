012017bb1ae36ffc530f226216a6da6059529632
Bhim Gurung <gbhimsingh@ymail.com>
2025-04-17 15:59:40 -0400

    Completed the Exercise 3: TestStatistics

---
+++ TestStatistics/src/main/java/com/pluralsight/TestStat.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+import java.util.Arrays;
+public class TestStat {
+    public static void main(String[] args) {
+        int[] TestScore = {100, 98, 90, 87, 85, 80, 75, 70, 68, 65};
+
+        System.out.println(totalTestScores/[testScores.length]);
+
+
+        int sum = 0;
+        int highScore = TestScore[0];
+
+        for (int score : TestScore) {
+            sum += score;
+        }
+
+}
